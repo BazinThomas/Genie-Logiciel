bc0ab4aa25da37ccf850d0e4520ca0ae907084ef
LeLunaa <38189825+LeLunaa@users.noreply.github.com>
2019-11-15 13:31:45 +0100

    Update Tracabilite.sql

--- Tracabilite.sql
+++ Tracabilite.sql
@@ -4,15 +4,16 @@ DROP TABLE Expedition;
 DROP TABLE Cellule;
 
 CREATE TABLE InfoCereale (
-    catégorie VARCHAR(50), /* Blé, orge, pois, proteagineux, colza */
-    poids number(2), /* moyenne en grammes */
-    qualité VARCHAR(50),  /* Mauvais, Bon, Excellent*/
+    Catégorie VARCHAR(50), /* Blé, orge, pois, proteagineux, colza */
+    Poids number(2), /* moyenne en grammes */
+    Qualité VARCHAR(50),  /* Mauvais, Bon, Excellent*/
     NoLot number(5), /* Numero du lot sur 5 chiffres max */
+    VolumeLot number(5), /*en volume unitaire t'es chiant si tu dépasse 1370 m3 */
     PRIMARY KEY (NoLot) 
 );
 
 CREATE TABLE Reception (
-    acheminement VARCHAR(50), /* Train, Routier, Bateau, Aérien */
+    Acheminement VARCHAR(50), /* Train, Routier, Bateau, Aérien */
     NoCellule number(2), /* Numéro de la cellule dans laquelle il va être stocké */
     NoRecepLot number(5),
     NoRecep number(5),
@@ -42,11 +43,11 @@ CREATE TABLE Expedition (
 );
 
 /* petit test d'intégration */
-INSERT INTO InfoCereale VALUES('Blé', 10, 'Bon', 225);
+INSERT INTO InfoCereale VALUES('Blé', 10, 'Bon', 225, 445);
 INSERT INTO Reception VALUES('Routier',2,225,1);
 
-INSERT INTO InfoCereale VALUES('Blé', 20, 'Mauvais', 226);
-INSERT INTO Cellule VALUES(current DATE,current DATE,current DATE,1300, 2, 226, 3);
+INSERT INTO InfoCereale VALUES('Blé', 6, 'Mauvais', 226, 1200);
+INSERT INTO Cellule VALUES(current DATE,current DATE,current DATE, 1300, 2, 226, 3);
 
-INSERT INTO InfoCereale VALUES('Blé', 5, 'Bon', 227);
+INSERT INTO InfoCereale VALUES('Blé', 10, 'Bon', 227, 4);
 INSERT INTO Reception VALUES('BazinMobile',35,'TonnerreCity',227,4);
