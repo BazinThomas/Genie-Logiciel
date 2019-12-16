3a73f92f0f8f48a8017e042b24c3516908bce6b0
Nicolas LARDI <nl435089@MI115-01.iem>
2019-12-16 14:20:08 +0100

    Ajout des lignes représentant la BD du silo et des cellules dans le fichier Silo.java

--- Silo.java
+++ Silo.java
@@ -1,4 +1,4 @@
-package fr.ufrsciencestech.panier;
+package Beuzelin;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,6 +19,19 @@ public class Silo
                 this.nbCellule=nbc;
                 this.hauteurCereale=hc;
 	}
+    
+    Silo _s1 = new Silo ("tout_type",10,1370);
+    
+    Cellule _cel1   = new Cellule ("blé", 410, false);
+    Cellule _cel2   = new Cellule ("blé", 410, false);
+    Cellule _cel3   = new Cellule ("blé", 570, false);
+    Cellule _cel4   = new Cellule ("orge", 498, false);
+    Cellule _cel5   = new Cellule ("poids", 984, false);
+    Cellule _cel6   = new Cellule ("poids", 651, false);
+    Cellule _cel7   = new Cellule ("colza", 1370, true);
+    Cellule _cel8   = new Cellule ("colza", 1370, true);
+    Cellule _cel9   = new Cellule ("colza", 1000, false);
+    Cellule _cel10  = new Cellule ("proteagineux", 570, false); 
     
     //Methodes
     public void Nettoyage(){
